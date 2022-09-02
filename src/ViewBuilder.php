dd745f475d2c35a2540d8e185e148fc2a0d94ae0
Node <8974108+qwenode@users.noreply.github.com>
2022-09-02 16:56:10 +0800

    add new

--- src/ViewBuilder.php
+++ src/ViewBuilder.php
@@ -3,9 +3,29 @@
 namespace qwenode\yii2lightning;
 
 use qwephp\assert\Assertion;
+use qwephp\StrHelper;
+use yii\base\InvalidConfigException;
 
 class ViewBuilder
 {
+    /**
+     * 链接自动高亮
+     * @param string $url
+     * @param string $appendClass
+     * @param string $activeClass
+     * @return string
+     * @throws InvalidConfigException
+     */
+    public static function activeLink(string $url, string $appendClass = '', string $activeClass = 'active')
+    {
+        $current = LightningHelper::getRequest()->getUrl();
+        $tpl     = ' href="%s" class="%s" ';
+        if (StrHelper::contain($current, $url)) {
+            return sprintf($tpl, $url, $appendClass . ' ' . $activeClass);
+        }
+        return sprintf($tpl, $url, $appendClass);
+    }
+    
     public static function inputDate($unixtime, ...$options): array
     {
         if ($unixtime <= 0) {
