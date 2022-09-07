643f1f537368fe7e07921ffb391c6363b4ac253d
Node <8974108+qwenode@users.noreply.github.com>
2022-09-07 17:37:22 +0800

    update

--- src/MenuBuilder.php
+++ src/MenuBuilder.php
@@ -2,36 +2,7 @@
 
 namespace qwenode\yii2lightning;
 
-class MenuBuilder
+class MenuBuilder extends ViewBuilder
 {
-    /**
-     * @var null|callable
-     */
-    public static $authorizeCallback = null;
-    
-    public static function item(string $label, $url, $authCode = null)
-    {
-        if (is_callable(self::$authorizeCallback) && $authCode !== null) {
-            if (!call_user_func(self::$authorizeCallback, $authCode)) {
-                return '';
-            }
-        }
-        return [
-            'label' => $label,
-            'url'   => $url,
-        ];
-    }
-    
-    public static function items(string $label, ...$items)
-    {
-        $list = [
-            'label' => $label,
-            'items' => [],
-        ];
-        foreach ($items as $item) {
-            if ($item == '') continue;
-            $list['items'][] = $item;
-        }
-        return $list;
-    }
+
 }
--- src/ViewBuilder.php
+++ src/ViewBuilder.php
@@ -8,6 +8,38 @@ use yii\base\InvalidConfigException;
 
 class ViewBuilder
 {
+    
+    /**
+     * @var null|callable
+     */
+    public static $authorizeCallback = null;
+    
+    public static function item(string $label, $url, $authCode = null)
+    {
+        if (is_callable(self::$authorizeCallback) && $authCode !== null) {
+            if (!call_user_func(self::$authorizeCallback, $authCode)) {
+                return '';
+            }
+        }
+        return [
+            'label' => $label,
+            'url'   => $url,
+        ];
+    }
+    
+    public static function items(string $label, ...$items)
+    {
+        $list = [
+            'label' => $label,
+            'items' => [],
+        ];
+        foreach ($items as $item) {
+            if ($item == '') continue;
+            $list['items'][] = $item;
+        }
+        return $list;
+    }
+    
     /**
      * 链接自动高亮
      * @param string $url
