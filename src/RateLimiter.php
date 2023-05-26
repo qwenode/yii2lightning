6642c6edb932dd425cea4154ba39862f7ff1a9cc
Node <8974108+qwenode@users.noreply.github.com>
2023-05-26 14:28:10 +0800

    RateLimiter.php

--- src/RateLimiter.php
+++ src/RateLimiter.php
@@ -3,6 +3,8 @@
 namespace qwenode\yii2lightning;
 
 use ErrorException;
+use qwephp\AA;
+use qwephp\SS;
 use yii\caching\Cache;
 use yii\caching\CacheInterface;
 use yii\caching\TagDependency;
@@ -25,18 +27,18 @@ class RateLimiter
      * @var CacheInterface|Connection|Cache
      */
     protected $_cache;
-    protected string $_uniqueIdentifier;
     protected int $_limit;
     protected int $_lifetime;
     protected bool $_isRedis = false;
     protected int $shouldBe;
-    public function __construct(string $collection, int $limit, int $minutes)
+    
+    protected function __construct(string $collection, int $limit, int $minutes)
     {
         if ($minutes < 1) {
             $minutes = 1;
         }
-        $this->_collection       = strtoupper(sprintf('LIGHTING_RATE_LIMITER:%s', sha1($collection)));
-        $this->_uniqueIdentifier = $this->_collection;
+        $collection              = SS::removeSpecialChar($collection);
+        $this->_collection       = strtoupper(sprintf('LIGHTING_RATE_LIMITER:%s', $collection));
         $this->_limit            = $limit;
         $this->_lifetime         = $minutes * 60;
         
@@ -46,70 +48,90 @@ class RateLimiter
         } else {
             $this->_cache = LightningHelper::getCache();
         }
-        $this->shouldBe = (int)$this->_cache->get($this->_uniqueIdentifier)+1;
+        $this->shouldBe = (int)$this->_cache->get($this->_collection) + 1;
     }
-
+    
     public static function withCollection(string $collection, int $limit = 5, int $minutes = 5): static
     {
         return new static($collection, $limit, $minutes);
     }
-
+    
     /**
-     * 最终的key=LIGHTING_RATE_LIMITER:collection:uniqueIdentifier+limit
-     * @param string $uniqueIdentifier 根据唯一ID进行限制
+     * 最终的key=LIGHTING_RATE_LIMITER:parent-collection:factor-collection
+     * @param string $collection 根据唯一ID进行限制
      * @param int $limit 此参数也会加入ID计算规则,同样的 $uniqueIdentifier 不同的 $limit 则 限制计数不会冲突
      * @param int $minutes
      * @return RateLimiter
      */
-    public function factor(string $uniqueIdentifier, int $limit, int $minutes = 5): RateLimiter
+    public function factor(string $collection, int $limit, int $minutes = 5): RateLimiter
     {
         if ($minutes < 1) {
             $minutes = 1;
         }
-        return new static(strtoupper(sprintf('%s:%s', $this->_collection,
-            sha1($uniqueIdentifier . $limit))),$limit,$minutes*60);
+        $collection  = SS::removeSpecialChar($collection);
+        $rateLimiter = clone $this;
+        $rateLimiter->setCollection(strtoupper(sprintf('%s:%s', $this->_collection, $collection)));
+        $rateLimiter->setLimit($limit);
+        $rateLimiter->setLifeTime($minutes * 60);
+        return $rateLimiter;
     }
-
+    
+    public function setLifeTime(int $lifeTime)
+    {
+        $this->_lifetime = $lifeTime;
+    }
+    
+    public function setLimit(int $limit)
+    {
+        $this->_limit = $limit;
+    }
+    
+    public function setCollection(string $value)
+    {
+        $this->_collection = $value;
+    }
+    
     public function hit(): void
     {
         if ($this->_isRedis) {
-            $next = (int)$this->_cache->incr($this->_uniqueIdentifier);
-            $this->_cache->expire($this->_uniqueIdentifier, $this->_lifetime);
+            $next = (int)$this->_cache->incr($this->_collection);
+            $this->_cache->expire($this->_collection, $this->_lifetime);
         } else {
-            $v = (int)$this->_cache->get($this->_uniqueIdentifier);
+            $v = (int)$this->_cache->get($this->_collection);
             $v++;
             $next = $v;
-            $this->_cache->set($this->_uniqueIdentifier, $v, $this->_lifetime, new TagDependency(['tags' => $this->_collection]));
+            $this->_cache->set($this->_collection, $v, $this->_lifetime, new TagDependency(['tags' => $this->_collection]));
         }
         if ($this->shouldBe != $next) {
             throw new RateLimitExceededException('Rate limit exceeded');
         }
     }
-
+    
     public function invalidateFactor(): void
     {
         if ($this->_isRedis) {
-            $this->_cache->del($this->_uniqueIdentifier);
+            $this->_cache->del($this->_collection);
         } else {
-            $this->_cache->delete($this->_uniqueIdentifier);
+            $this->_cache->delete($this->_collection);
         }
     }
-
+    
     public function invalidateCollection(): void
     {
         if ($this->_isRedis) {
             $this->_cache->del(sprintf('%s:*', $this->_collection));
+            $this->_cache->del($this->_collection);
         } else {
             TagDependency::invalidate($this->_cache, $this->_collection);
         }
     }
-
+    
     public function verify(): bool
     {
-        $v = (int)$this->_cache->get($this->_uniqueIdentifier);
+        $v = (int)$this->_cache->get($this->_collection);
         return $v < $this->_limit;
     }
-
+    
     /**
      * @return void
      * @throws RateLimitExceededException
